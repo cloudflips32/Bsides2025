1a2bc3a457aecb8a6c8d6e75bceb1e0efc60ea4b
cloudflips32 <devadamstandish@gmail.com>
2025-05-13 03:06:02 -0400

    cards readjusted, navbar is black again

--- src/app/components/info-cards.jsx
+++ src/app/components/info-cards.jsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 
 export default function InfoCards() {
   return (
-    <div className="container mx-auto px-4 py-16">
-      <div className="flex flex-wrap md:flex-row justify-center gap-8">
+    <div className="container mx-auto px-4 py-16 mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Sponsors Card */}
         <Card>
           <CardHeader>
