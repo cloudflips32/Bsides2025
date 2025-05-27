a314bea199243355c35bded83f2c45cab124e9b2
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-27 15:33:02 -0400

    in About us, added main pages square

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -2,7 +2,8 @@ import Hero from "../components/hero"
 export default function AboutUs() {
  return (<>
 <main className="max-w-5xl mx-auto my-6 p-4">
-<Hero> </Hero>
+<div className= "relative bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500 py-24 md:py-30">
+</div>
 </main>
 </>)
 }
