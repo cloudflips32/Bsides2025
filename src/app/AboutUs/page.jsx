c05ff8fdcb73c77782ee9b2c522ad5c2c3964ee1
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-27 15:48:25 -0400

    added the decorative elements from the Home page

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -1,9 +1,20 @@
 import Hero from "../components/hero"
 export default function AboutUs() {
- return (<>
-<main className="max-w-5xl mx-auto my-6 p-4">
-<div className= "relative bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500 py-24 md:py-30">
+ return (<> <main className="max-w-5xl mx-auto my-6 p-4"> 
+{/*Background box everything sits on*/}
+ <div className= "relative 
+bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500 
+py-24 md:py-30">
+ {/*Decorative elements*/}
+	<div className="absolute top-1/4 left-1/6 w-20 h-20 bg-orange-400 rounded-full opacity-20"></div>
+	<div className="absolute top-1/3 left-2/3 w-20 h-20 bg-orange-400 rounded-full opacity-20"></div>
+	<div className="absolute top-3/4 left-4/6 w-20 h-20 bg-orange-400 rounded-full opacity-20"></div>
+{/* Sparkles */}
+<div className="absolute top-5/6 left-2/3 w-2 h-2 bg-white rounded-full opacity-70"></div>
 </div>
+<div className="absolute bottom-1/4 left-2/3 w-2 h-2 bg-white rounded-full opacity-70"></div>
+<div className="absolute top-1/6 left-1/3 w-2 h-2 bg-white rounded-full opacity-70"></div>
+{/*Decorative Element End*/}
 </main>
 </>)
 }
