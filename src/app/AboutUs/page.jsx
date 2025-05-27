43f785ce20b2cc0b6982d3c3e6c3d457592b3485
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-27 16:14:36 -0400

    removed Decorative elements

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -7,9 +7,9 @@ bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500
 py-24 md:py-30">
 <div class="flex">
  {/*Decorative elements*/}
-	<div className="absolute w-20 h-20 bg-orange-400 rounded-full opacity-20"></div>
-	<div className="absolute w-20 h-20 bg-orange-400 rounded-full opacity-20"></div>
-	<div className="absolute w-20 h-20 bg-orange-400 rounded-full opacity-20"></div>
+	<div className="bg-orange-400"></div>
+	<div className="bg-yellow-400"></div>
+	<div className="bg-purple-400"></div>
 </div>
 </div>
 </main>
