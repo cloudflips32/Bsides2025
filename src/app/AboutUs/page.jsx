5c18f963f2e97e828d4da7c177a58e85bb6b8051
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-28 13:41:20 -0400

    Added a 'What to expect' segment

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -13,7 +13,19 @@ py-24 md:py-30">
 	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
 		<p className="text-6xl">Our Mission</p>
 		<p className="flex-1">Our mission is to provide a welcoming, inclusive environment where people of all skill levels can connect, share knowledge, and collaborate on solutions to today's cybersecurity challenges. We believe in lowering barries to entry, uplifting new voices, and creating opportunities to grow and give back to the community.</p>
-	</li>		
+	</li>	
+	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
+		<p className="text-6xl">What to Expect</p>
+		<p className="flex-1">Bsides SWFL is a conference built by the community, for the community. We offer: 
+		<ul className="list-disc">
+			<li><strong>Enagaging Talks</strong> on timely and relevant cybersecurity topics</li>
+			<li><strong>Interactive Villages</strong> and hands-on activities</li>
+			<li><strong>Workshops</strong> that let attendees apply what they learn</li>
+			<li><strong>Opportunites to Connect</strong> with peers, mentors, and industry experts</li>
+		</ul>
+		Whether you're just beginning your journey or you've been in the industry for year,s BSides SWFL is a place where you can share, learn, and be part of something meaningful.
+		</p>
+	</li>				
 	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
 		<p className="text-6xl">Why It Matters</p>
 		<p className="flex-1">Tridiional conferences often come with barriers: cost, exclusivity, or limited speaker slots. BSides flips the script by offering a platform where more voices can be hear, and more can participate. We're here to celebrate curiosity, encourage growth, and help build a stronger, more resilient security community in Southwest Florida.</p>
