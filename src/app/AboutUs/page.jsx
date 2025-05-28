e900ec17e98454d0ef831affcae2fb07a0e7fdef
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-28 13:10:35 -0400

    added the text desired to first header

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -7,9 +7,10 @@ bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500
 py-24 md:py-30">
 <ul>
 	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
-		<p className="text-6xl">Header</p>
-		<p className="flex-1">This should be text that we can see as a main point of the header. The rest of the text is just filler text to see how word wrapping works on this element, so enjoy the long text</p>
+		<p className="text-6xl">About Us</p>
+		<p className="flex-1"><strong>BSides Southwest Florida (Bsides SWFL) </strong> is a communtiy-driven cybersecurity conference designed to create space for open conversation, hands-on learning, and professional growth. As part of the global <a>Security BSides</a> movement, our event brings together a diverse mix of security professionals, students, researchers, and curious minds to explore and advance the field of information security.</p>
 	</li>
+	
 </ul>
 </div>
 </main>
