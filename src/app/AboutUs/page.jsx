94ca5011b363e207892cafefec0be52cbbfb9f27
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-28 13:55:26 -0400

    made all headers a smaller size

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -7,15 +7,15 @@ bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500
 py-24 md:py-30">
 <ul>
 	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
-		<p className="text-6xl">About BSides SWFL</p>
+		<p className="text-4xl">About BSides SWFL</p>
 		<p className="flex-1"><strong>BSides Southwest Florida (Bsides SWFL) </strong> is a communtiy-driven cybersecurity conference designed to create space for open conversation, hands-on learning, and professional growth. As part of the global <a>Security BSides</a> movement, our event brings together a diverse mix of security professionals, students, researchers, and curious minds to explore and advance the field of information security.</p>
 	</li>
 	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
-		<p className="text-6xl">Our Mission</p>
+		<p className="text-4xl">Our Mission</p>
 		<p className="flex-1">Our mission is to provide a welcoming, inclusive environment where people of all skill levels can connect, share knowledge, and collaborate on solutions to today's cybersecurity challenges. We believe in lowering barries to entry, uplifting new voices, and creating opportunities to grow and give back to the community.</p>
 	</li>	
 	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
-		<p className="text-6xl">What to Expect</p>
+		<p className="text-4xl">What to Expect</p>
 		<p className="flex-1">Bsides SWFL is a conference built by the community, for the community. We offer: 
 		<ul className="text-left indent-3 list-disc">
 			<li><strong>Enagaging Talks</strong> on timely and relevant cybersecurity topics.</li>
@@ -27,7 +27,7 @@ py-24 md:py-30">
 		</p>
 	</li>				
 	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
-		<p className="text-6xl">Why It Matters</p>
+		<p className="text-4xl">Why It Matters</p>
 		<p className="flex-1">Tridiional conferences often come with barriers: cost, exclusivity, or limited speaker slots. BSides flips the script by offering a platform where more voices can be hear, and more can participate. We're here to celebrate curiosity, encourage growth, and help build a stronger, more resilient security community in Southwest Florida.</p>
 	</li>
 </ul>
