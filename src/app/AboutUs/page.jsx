11eeb1f4f18834bfb182f0270c6af3ccf7cd113b
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-28 13:01:05 -0400

    Added a gradient for the list element

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -6,7 +6,7 @@ export default function AboutUs() {
 bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500 
 py-24 md:py-30">
 <ul>
-	<li className="flex flex-row bg-orange-400 justify-between space-x-8">
+	<li className="flex flex-row justify-between space-x-8 bg-gradient-to-r from-orange-300  via-orange-400 to-orange-500">
 		<p className="text-6xl">Header</p>
 		<p className="flex-1">This should be text that we can see as a main point of the header. The rest of the text is just filler text to see how word wrapping works on this element, so enjoy the long text</p>
 	</li>
