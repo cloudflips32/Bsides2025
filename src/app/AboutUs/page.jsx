ec902854ae7a2eb2ed4431543f7ad35a1c30bbce
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-05-29 19:42:54 -0400

    centered the rest and added padding to the rest

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -13,9 +13,9 @@ export default function AboutUs() {
 		<div className="text-center justify-center"><p className="text-4xl">Our Mission</p></div>
 		<div><p className="flex-1">Our mission is to provide a welcoming, inclusive environment where people of all skill levels can connect, share knowledge, and collaborate on solutions to today's cybersecurity challenges. We believe in lowering barriers to entry, uplifting new voices, and creating opportunities to grow and give back to the community.</p></div>
 	</li>	
-	<li className="flex flex-row justify-between space-x-8 mt-6 border-solid border-1 rounded-xl shadow-sm border-gray-950">
-		<div><p className="text-4xl">What to Expect</p></div>
-		<div><p className="flex-1">Bsides SWFL is a conference built by the community, for the community. We offer: 
+	<li className="p-4 flex flex-row justify-between space-x-8 mt-6 border-solid border-1 rounded-xl shadow-sm border-gray-950">
+		<div className="text-center justify-center"><p className="text-4xl">What to Expect</p></div>
+		<div className="text-center justify-center"><p className="flex-1">Bsides SWFL is a conference built by the community, for the community. We offer: 
 		<ul className="text-left indent-3 list-disc">
 			<li><strong>Engaging Talks</strong> on timely and relevant cybersecurity topics.</li>
 			<li><strong>Interactive Villages</strong> and hands-on activities.</li>
@@ -25,9 +25,9 @@ export default function AboutUs() {
 		Whether you're just beginning your journey or you've been in the industry for years, BSides SWFL is a place where you can share, learn, and be part of something meaningful.
 		</p></div>
 	</li>				
-	<li className="flex flex-row justify-between space-x-8 mt-6 border-solid border-1 rounded-xl shadow-sm border-gray-950">
-		<div><p className="text-4xl">Why It Matters</p></div>
-		<div><p className="flex-1">Traditional conferences often come with barriers: cost, exclusivity, or limited speaker slots. BSides flips the script by offering a platform where more voices can be heard, and more people can participate. We're here to celebrate curiosity, encourage growth, and help build a stronger, more resilient security community in Southwest Florida.</p></div>
+	<li className="p-4 flex flex-row justify-between space-x-8 mt-6 border-solid border-1 rounded-xl shadow-sm border-gray-950">
+		<div className="text-center justify-center"><p className="text-4xl">Why It Matters</p></div>
+		<div className="text-center justify-center"><p className="flex-1">Traditional conferences often come with barriers: cost, exclusivity, or limited speaker slots. BSides flips the script by offering a platform where more voices can be heard, and more people can participate. We're here to celebrate curiosity, encourage growth, and help build a stronger, more resilient security community in Southwest Florida.</p></div>
 	</li>
 </ul>
 </div>
