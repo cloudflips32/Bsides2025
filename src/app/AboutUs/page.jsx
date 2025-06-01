9cd63156d0ecb5b8dcb3d88d1e2ade175975f377
Christopher Gonzalez <gonzalezc9999@gmail.com>
2025-06-01 18:00:31 -0400

    What to expect mobile is now truncated first, expanded after

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -57,23 +57,26 @@ return (<> <main className="max-w-5xl mx-auto my-6 p-4">
 	<div><p className="overflow-hidden truncate max-h-180 justify-center"><strong>BSides Southwest Florida (Bsides SWFL) </strong> is a community-driven cybersecurity conference designed to create space for open conversation, hands-on learning, and professional growth. As part of the global <a href="#">Security BSides</a> movement, our event brings together a diverse mix of security professionals, students, researchers, and curious minds to explore and advance the field of information security.</p></div>	
 )}	
 {AboutUs && (
-	<div><p className="max-h-180 justify-center"><strong>BSides Southwest Florida (Bsides SWFL) </strong> is a community-driven cybersecurity conference designed to create space for open conversation, hands-on learning, and professional growth. As part of the global <a href="#">Security BSides</a> movement, our event brings together a diverse mix of security professionals, students, researchers, and curious minds to explore and advance the field of information security.</p></div>	
+	<div><p className="max-Zh-180 justify-center"><strong>BSides Southwest Florida (Bsides SWFL) </strong> is a community-driven cybersecurity conference designed to create space for open conversation, hands-on learning, and professional growth. As part of the global <a href="#">Security BSides</a> movement, our event brings together a diverse mix of security professionals, students, researchers, and curious minds to explore and advance the field of information security.</p></div>	
 )}</li>
 	<li className="p-4 flex flex-col justify-between space-x-8 border-solid border-1 rounded-xl shadow-sm border-gray-950">
 		<div onClick={toggleOurMission} className="text-center justify-center"><p className="turncate text-4xl">Our Mission</p></div>
+	{!OurMission && (
+		<div><p className="overflow-hidden truncate max-h-180 justify-center">Our mission is to provide a welcoming, inclusive environment where people of all skill levels can connect, share knowledge, and collaborate on solutions to today's cybersecurity challenges. We believe in lowering barriers to entry, uplifting new voices, and creating opportunities to grow and give back to the community</p></div>)}
 	{OurMission && (
-	<div><p className="overflow-hidden truncate max-h-180 justify-center">Our mission is to provide a welcoming, inclusive environment where people of all skill levels can connect, share knowledge, and collaborate on solutions to today's cybersecurity challenges. We believe in lowering barriers to entry, uplifting new voices, and creating opportunities to grow and give back to the community</p></div>)}</li>
+		<div><p className="max-h-180 justify-center">Our mission is to provide a welcoming, inclusive environment where people of all skill levels can connect, share knowledge, and collaborate on solutions to today's cybersecurity challenges. We believe in lowering barriers to entry, uplifting new voices, and creating opportunities to grow and give back to the community</p></div>)}</li>
 	<li className="p-4 flex flex-col justify-between space-x-8 border-solid border-1 rounded-xl shadow-sm border-gray-950">
 		<div onClick={toggleWhatToExpect} className="text-center justify-center"><p className="turncate text-4xl">What To Expect</p></div>
+	{!WhatToExpect && (<div><p className="overflow-hidden truncate max-h-180 justify-center">BSides SWFL offers <strong>Engaging Talks, Interactive Villages, Workshops, Opportunites to connect</strong></p></div>)}
 	{WhatToExpect && (
-	<div><p className="overflow-hidden truncate max-h-180 justify-center">Bsides SWFL is a conference built by the community, for the community. We offer:</p> 
+		<div><p className="max-h-180 justify-center">Bsides SWFL is a conference built by the community, for the community. We offer:</p> 
 		<ul className="text-left indent-3 list-disc">
 			<li><strong>Engaging Talks</strong> on timely and relevant cybersecurity topics.</li>
 			<li><strong>Interactive Villages</strong> and hands-on activities.</li>
 			<li><strong>Workshops</strong> that let attendees apply what they learn.</li>
 			<li><strong>Opportunities to Connect</strong> with peers, mentors, and industry experts.</li>
 		</ul>
-		<p className="overflow-hidden truncate max-w-180 flex-1">Whether you're just beginning your journey or you've been in the industry for years, BSides SWFL is a place where you can share, learn, and be part of something meaningful.
+		<p className="max-w-180 flex-1">Whether you're just beginning your journey or you've been in the industry for years, BSides SWFL is a place where you can share, learn, and be part of something meaningful.
 		</p>
 	</div>)}
 	</li>
