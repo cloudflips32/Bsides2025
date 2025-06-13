a3c12caab264d12181fbcd6da62147534494077e
cloudflips32 <devadamstandish@gmail.com>
2025-06-13 19:45:07 -0400

    contactus edit

--- src/app/contactus/page.jsx
+++ src/app/contactus/page.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const ContactUs = () => {
   return (
     <div>
-      <script src="https://js-na2.hsforms.net/forms/embed/242985282.js async"></script>
       <div className="hs-form-frame" data-region="na2" data-form-id="e30cd9a7-5c69-4b8b-9068-4e9f694a9dd4" data-portal-id="242985282"></div>
+      <script src="https://js-na2.hsforms.net/forms/embed/242985282.js async defer"></script>
     </div>
   )
 }
