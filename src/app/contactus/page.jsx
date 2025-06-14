9678a09ca5ceb3afc2944457c08b61cd0be43817
cloudflips32 <devadamstandish@gmail.com>
2025-06-13 21:40:08 -0400

    ContactUs edits

--- src/app/contactus/page.jsx
+++ src/app/contactus/page.jsx
@@ -4,7 +4,7 @@ const ContactUs = () => {
   return (
     <div>
       <div className="hs-form-frame" data-region="na2" data-form-id="e30cd9a7-5c69-4b8b-9068-4e9f694a9dd4" data-portal-id="242985282"></div>
-      <script src="https://js-na2.hsforms.net/forms/embed/242985282.js async"></script>
+      <script src="https://js-na2.hsforms.net/forms/embed/242985282.js"></script>
     </div>
   )
 }
