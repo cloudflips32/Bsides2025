c6b4dfd5290f28aa3642a9af89797450052ff315
cloudflips32 <devadamstandish@gmail.com>
2025-06-20 00:23:07 -0400

    call for speakers, calendar icons

--- src/app/components/call-for-speakers.jsx
+++ src/app/components/call-for-speakers.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Calendar } from 'lucide-react';
 
 const CallForSpeakersModal = ({ isOpen, onClose }) => {
   if (!isOpen) {
@@ -16,10 +17,10 @@ const CallForSpeakersModal = ({ isOpen, onClose }) => {
         </button>
         <div className="m-auto md:h-[78vh] flex flex-col md:flex-row md:items-center md:justify-between rounded-2xl">
           <div className="bg-gradient-to-br from-amber-100 via-orange-100 to-red-100 rounded-lg shadow-lg p-6 md:p-8 w-full max-w-2xl">
-            <div className="flex justify-between items-center mb-6">
+            <div className="flex justify-between mt-1 items-center mb-6">
               <h1 className="text-2xl font-semibold text-teal-700">Call for Speakers</h1>
-              <span className="bg-green-100 text-teal-700 text-sm w-auto font-medium px-3 py-1 rounded-full">
-                4 months left
+              <span className="bg-green-100 text-teal-700 text-sm w-[35vw] md:w-auto mx-3 text-center font-medium p-1 rounded-full">
+                <p className='inline-block px-2'><Calendar className="mr-1 mb-1 inline-block" width={15} height={15} />Nov 14th</p>
               </span>
             </div>
 
@@ -40,10 +41,7 @@ const CallForSpeakersModal = ({ isOpen, onClose }) => {
                 <p>Closing time in your timezone (America/New_York) is <span className="font-semibold">30 Sep 2025 11:59 pm</span>.</p>
               </div>
               <button className="bg-gray-200 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-300 transition-colors duration-200 flex items-center justify-center md:justify-start">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 m-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
-                <p className="text-sm">Add to calendar</p>
+                <p className="text-sm"><Calendar className="mr-1 mb-1 inline-block" width={15} height={15} /> Add to calendar</p>
               </button>
             </div>
 
